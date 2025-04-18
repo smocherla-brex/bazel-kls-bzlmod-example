bd997d03e54d2681e23687d5305dc5ec708910fe
Sridhar Mocherla <smocherla@brex.com>
2025-04-18 13:03:21 -0400

    Update to bazel8

--- src/main/kotlin/com/foo/helloworld/HelloController.kt
+++ src/main/kotlin/com/foo/helloworld/HelloController.kt
@@ -2,7 +2,6 @@ package com.foo.helloworld
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.MediaType
 import com.foo.helloworld.proto.Foo
